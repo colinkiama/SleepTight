43d2f781b02f8ae24cb32b2d0e24744df672a7db
Colin Kiama <colinkiama@hotmail.co.uk>
2018-04-19 01:33:01 +0100

    Currently working on gettig data from controls when adding sleep data

--- Sleepy/View/AddSleepDataView.xaml.cs
+++ Sleepy/View/AddSleepDataView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -30,7 +31,21 @@ namespace Sleepy.View
 
         private void FinishAddingSleepDataButton_Click(object sender, RoutedEventArgs e)
         {
+            // Getting start of sleep
+            DateTimeOffset startDate = SleepStartDatePicker.Date;
+            TimeSpan startTime = SleepEndTimePicker.Time;
+            DateTime sleepStart = new DateTime(startDate.Year, startDate.Month, startDate.Day,startTime.Hours,startTime.Minutes,0);
 
+            // Getting end of sleep 
+            DateTimeOffset endDate = SleepEndDatePicker.Date;
+            TimeSpan endTime = SleepEndTimePicker.Time;
+            DateTime sleepEnd = new DateTime(endDate.Year, endDate.Month, endDate.Day, endTime.Hours, endTime.Minutes, 0);
+
+            // Getting quality of sleep
+            int ratingValueAsInt = (int)SleepQualityRatingControl.Value;
+            // TODO: Case Statement for values 1 to 5 paired to 1 to 5 star sleep quality.
+
+            
         }
     }
 }
